d2a619a17363975d0383d3c1196a9e3702479933
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2018-12-09 12:29:50 +0530

    java code

---
+++ server/Beer.java
@@ -0,0 +1,45 @@
+package com.prakash.developer.demo.beer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Beer {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Beer() {}
+
+    public Beer(String name) { this.name = name;}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Beer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
---
+++ server/BeerController.java
@@ -0,0 +1,43 @@
+package com.prakash.developer.demo.beer;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@RestController
+public class BeerController {
+
+    private BeerRepository repository;
+
+    public BeerController (BeerRepository repository) {
+
+        this.repository = repository;
+
+    }
+
+
+    @GetMapping("/good-beers")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public Collection<Beer> goodBeer () {
+
+        return repository.findAll().stream().filter(this::isGreat).collect(Collectors.toList());
+
+    }
+
+    private boolean isGreat(Beer beer) {
+
+        return !beer.getName().equals("Budweiser") &&
+                !beer.getName().equals("Coors Light") &&
+                !beer.getName().equals("PBR");
+
+    }
+
+
+}
